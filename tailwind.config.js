225a0294af98a23277aa3b932550acf1eab1fe25
Hien Nguyen Trong <nguyenhien.meek@gmail.com>
2024-12-31 14:55:24 +0700

    chore: add layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: "#FFFFFF",
+        primary: {
+          300: "#FFCC21",
+          400: "#FF963C",
+          500: "#EA6C00"
+        },
+        secondary: {
+          300: "#8FE9D0"
+        },
+        dark: {
+          500: "#414141",
+          600: "#2E2E2E"
+        },
+        gray: {
+          400: "#777777"
+        }
+      }
+    },
   },
   plugins: [],
 }
